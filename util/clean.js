72101c6680908f7be0c71780d8ac5c3d608158bd
Leif W <warp9pnt9@gmail.com>
2020-07-18 21:28:16 -0400

    Remove silly utils, package deps and scripts.

--- util/clean.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-const fse = require('fs-extra');
-fse.remove('dist');
--- util/tidy.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/*
-const fse = require('fs-extra');
-
-fse.move('dist/redirects/index.html', 'dist/_redirects')
-	.then(() => fse.remove('dist/redirects'))
-	.catch(err => console.error(err));
-*/
